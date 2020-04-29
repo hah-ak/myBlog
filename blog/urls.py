aff18dfd517045306b659a3d877c0aa5e7904ff5
KYC <skekdnl12@gmail.com>
2020-04-29 20:00:10 +0900

    2020-04-29
    url,views 수정
    
    pagination 함수화
    제너릭클래스 뷰 상속을 이용한 views변화
    맞춰서 url변화

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('<slug:sort>/<int:pk>/', views.CodeDetailView.as_view(), name='codes-detail'),
-    path('SSEA/', views.SSEAView.as_view(), name='SSEA'),
-    path('programmers/',views.programmersView.as_view(),name='programmers'),
+    path('Algorithm/<slug:sort>/<int:pk>/', views.CodeDetailView.as_view(), name='codes-detail'),
+    path('Algorithm/',views.AlgorithmVeiw.as_view(),name='Algorithm'),
+    path('Algorithm/SSEA/', views.SSEAView.as_view(), name='SSEA'),
+    path('Algorithm/programmers/',views.programmersView.as_view(),name='programmers'),
     path('fashion/', views.fashion, name='fashion'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name = 'about'),
-    path('result/',views.SearchView.as_view(),name='search'),
+    path('result/',views.Search,name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,25 @@ from django.views import generic
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Algorithm
+from .forms import search_forms
 
-
+# paging 메소드
+def paging(self,context):
+    paginator = context['paginator']
+    page_number_range = 5
+    max_index = len(paginator.page_range)
+    
+    page = self.request.GET.get('page')
+    current_page = int(page) if page else 1
+    start_index = int((current_page-1)//page_number_range)*page_number_range
+    end_index = start_index+page_number_range
+    if end_index >= max_index:
+        end_index = max_index
+    
+    page_range = list(range(start_index+1,end_index+1))
+    context['page_range'] = page_range
+    
+# mainpage 함수로 구현
 def main(request):
     Algorithms = Algorithm.objects.order_by('created_date')
     
@@ -37,81 +54,70 @@ class CodeDetailView(generic.DetailView):
 #     context = {'post':post,'index':index}
 #     return render(request, 'blog/codes.html', context)
 
-class SSEAView(generic.ListView):
-    paginate_by = 5
-    template_name = 'blog/SSEA.html'
-    context_object_name = 'Algorithms'
-    def get_queryset(self):
-        queryset = Algorithm.objects.filter(site = 'SSEA')
-        queryset = queryset.order_by('created_date')
-        return queryset
+
+class AlgorithmTemplateVeiw(generic.TemplateView):
+    template_name = 'blog/Algorithm_list.html'
     
+    def get_context_data(self, **kwargs):
+        context = super(AlgorithmTemplateVeiw, self).get_context_data(**kwargs)
+        context[object_list] = ['Algorithm']
+        return context
+    
+    
+class AlgorithmVeiw(generic.ListView):
+    model = Algorithm
+    queryset = Algorithm.objects.order_by('created_date')
+    paginate_by = 5
+    def get_context_data(self, **kwargs):
+        context = super(AlgorithmVeiw, self).get_context_data(**kwargs)
+        paging(self,context)
+        return context
+    
+    
+
+class SSEAView(generic.ListView):
+    queryset = Algorithm.objects.filter(site = 'SSEA')
+    queryset = queryset.order_by('created_date')
+    paginate_by = 5
     def get_context_data(self, **kwargs):
         context = super(SSEAView, self).get_context_data(**kwargs)
-        paginator = context['paginator']
-        page_number_range = 5
-        max_index = len(paginator.page_range)
-        
-        page = self.request.GET.get('page')
-        current_page = int(page) if page else 1
-        start_index = int((current_page-1)//page_number_range)*page_number_range
-        end_index = start_index+page_number_range
-        if end_index >= max_index:
-            end_index = max_index
-        
-        page_range = list(range(start_index+1,end_index+1))
-        context['page_range'] = page_range
+        paging(self,context)
         return context
     
-
-
-class programmersView(generic.ListView):
-    paginate_by = 5
-    template_name = 'blog/programmers.html'
-    context_object_name = "Algorithms"
-    def get_queryset(self):
-        queryset = Algorithm.objects.filter(site = 'programmers')
-        queryset = queryset.order_by('created_date')
-        return queryset
     
+class programmersView(AlgorithmVeiw ,generic.ListView):
+    queryset = Algorithm.objects.filter(site = 'programmers')
+    queryset = queryset.order_by('created_date')
+    paginate_by = 5
     def get_context_data(self, **kwargs):
         context = super(programmersView, self).get_context_data(**kwargs)
-        paginator = context['paginator']
-        page_number_range = 5
-        max_index = len(paginator.page_range)
-        
-        page = self.request.GET.get('page')
-        current_page = int(page) if page else 1
-        start_index = int((current_page-1)//page_number_range)*page_number_range
-        end_index = start_index+page_number_range
-        if end_index >= max_index:
-            end_index = max_index
-        
-        page_range = list(range(start_index+1,end_index+1))
-        context['page_range'] = page_range
+        paging(self,context)
         return context
+
     
 # def SSEA(request):
 #     posts = Post.objects.order_by('created_date')
 #     context = {'posts' : posts, 'index' : 'SSEA'}
 #     return render(request, 'blog/SSEA.html',context)
 
-class SearchView(generic.ListView):
-    template_name = 'blog/search.html'
-    context_object_name = 'results'
-    search = ""
-    def get_queryset(self):
-        # queryset = super(SearchView, self).get_queryset()
-        search = self.request.GET['search']
-        if search:
-            queryset = Algorithm.objects.filter(title__contains=search)
-        return queryset
+# class SearchView(generic.ListView):
+#     template_name = 'blog/search.html'
+#     context_object_name = 'results'
+#     def get_queryset(self):
+#         # queryset = super(SearchView, self).get_queryset()
+#         # search = self.request.GET['search']
+#         search = search_forms(self.request.GET)
+#         if search.is_valid():
+#             queryset = Algorithm.objects.filter(title__contains=search)
+#         return queryset
+
+def Search(request):
+    search = request.GET['search']
+    results = Algorithm.objects.filter(title__contains=search)
+    return render(request,'blog/search.html', {'results':results,'notfound':search})
     
-    def get_context_data(self, **kwargs):
-        context = super(SearchView, self).get_context_data(**kwargs)
-        search = self.request.GET['search']
-        context['notfound'] = search
-        return context
+    
+    
     
     
 def fashion(request):
